a8880daecb3f22be87ba7f3f2f88a997ab5a3465
fiaduz <123727054+fiaduz@users.noreply.github.com>
2023-08-26 21:36:00 +0530

    Update greater_less.c

--- greater_less.c
+++ greater_less.c
@@ -1,1 +1,20 @@
-#
+#include<stdio.h>
+void main()
+{
+  int a,b;
+  printf("Enter a number: ");
+  scanf("%d",&a);
+  printf("Enter another number: ");
+  scanf("%d",&b);
+
+  if(a>b)
+  {
+    printf("%d is greater",a);
+  }
+  else if(a<b)
+  {
+    printf("%d is greater",b);
+  }
+  else
+    printf("%d and %d both are equal",a,b);
+}  
